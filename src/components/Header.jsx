a1c96eb97134754c3c5c138566b4bfc81ea1855d
Lamperkat <elnur.zhubanov@gmail.com>
2022-08-05 17:25:35 +0600

    header component added

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Questionnaire</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/login">
+            <FaSignInAlt />
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/login">
+            <FaSignOutAlt />
+            Log out
+          </Link>
+        </li>
+        <li>
+          <Link to="/login">
+            <FaUser />
+            Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
